5afe3944048f5a7297123c2c4b45139015259294
Alejandro Avilés <omegak@gmail.com>
2016-04-16 15:10:00 +0200

    Implement tutorial

---
+++ src/app.c
@@ -0,0 +1,32 @@
+#include <pebble.h>
+
+Window *window;
+TextLayer *text_layer;
+
+void init() {
+  window = window_create();
+  text_layer = text_layer_create(GRect(0, 0, 144, 40));
+  text_layer_set_text(text_layer, "Hello, Pebble!");
+  layer_add_child(window_get_root_layer(window), 
+                  text_layer_get_layer(text_layer));
+  window_stack_push(window, true);
+}
+
+void deinit() {
+  text_layer_destroy(text_layer);
+  window_destroy(window);
+}
+
+int main() {
+  // Initialize the app
+  init();
+
+  // Wait for app events
+  app_event_loop();
+
+  // Deinitialize the app
+  deinit();
+
+  // App finished without error
+  return 0;
+}
